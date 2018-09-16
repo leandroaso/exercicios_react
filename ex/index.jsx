dad438913fbe86db479ee2dafa39cb0e86c10ae2
Leandro Alves <leandro.aso123@outlook.com>
2018-09-16 12:33:38 -0300

    Exercicios

---
+++ ex/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Family from './family'
+import Mamber from './member'
+
+ReactDOM.render(
+    <Family>
+        <Mamber name='leandro' lastName='Alves'/>
+    </Family>
+    ,document.getElementById('app'))
